d21fdf64b7f18a380a6bcbdb0cfcbcee1bb3fb43
Dylan <dylan.heirstraeten@epitech.eu>
2015-11-09 11:36:09 +0100

    add def trigger

--- src/components/blocks/modal.js
+++ src/components/blocks/modal.js
@@ -10,12 +10,15 @@ var Modal = React.createClass({
     getInitialState: function() {
         var style = {};
         var content = (this.props.content != undefined) ? React.createFactory(this.props.content) : React.createFactory(Default);
+        var trigger = (this.props.showTrigger != undefined) ? this.props.showTrigger : true;
         style.width = (this.props.width != undefined) ? this.props.width : "";
         style.height = (this.props.height != undefined) ? this.props.height : "";
         return ({
             className: "warm-component modal",
             style: style,
-            content: content
+            content: content,
+            trigger: trigger,
+            triggerState: false
         });
     },
     componentWillReceiveProps: function (nextProps) {
@@ -30,6 +33,22 @@ var Modal = React.createClass({
         }
         return true;
     },
+    triggerModal: function (e) {
+        console.log(e);
+        if(e.target.id == 'modal-trigger' || e.target.parentElement.id == 'modal-trigger') {
+            if (this.state.triggerState == true) {
+                this.setState({
+                    className: "warm-component modal",
+                    triggerState: false
+                });
+            } else {
+                this.setState({
+                    className: "warm-component modal active",
+                    triggerState: true
+                });
+            }
+        }
+    },
     propTypes: {
         isActive: React.PropTypes.bool,
         showTrigger: React.PropTypes.bool,
@@ -39,13 +58,26 @@ var Modal = React.createClass({
     },
     render: function () {
         console.log(this.state.content);
-        return (
-            <div className={this.state.className}>
-                <div className="modal-container" style={this.state.style}>
-                    {this.state.content()}
+        if (this.state.trigger == true) {
+            return (
+                <div className={this.state.className}>
+                    <div className="modal-container" style={this.state.style}>
+                        {this.state.content()}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div>
+                    <div id="modal-trigger" className="modal-trigger" onClick={this.triggerModal}></div>
+                    <div className={this.state.className}>
+                        <div className="modal-container" style={this.state.style}>
+                            {this.state.content()}
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     }
 });
 
